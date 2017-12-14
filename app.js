053591c2f83e3e51dd4814f70e94fe252db9c8c8
Neil Cooper <30900764+ncooper-amp@users.noreply.github.com>
2017-12-14 16:25:30 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -2,19 +2,21 @@ var express = require('express')
   , amp = require('cms-javascript-sdk')
   , app = express()
   , expressHbs = require('express-handlebars')
-  , hbs = require('hbs')
-  , axios = require('axios');
+  , axios = require('axios')
+  , helpers = require('handlebars-helpers')(['array', 'object']);
 
 app.use("/static",express.static(__dirname + '/static/'))
 app.set('view engine', 'hbs')
 
-hbs.registerPartials(__dirname + '/views/partials');
+
 
 app.engine('hbs', expressHbs( {
   extname: 'hbs',
   layoutsDir: __dirname + '/views/',
-  partialsDir: __dirname + '/views/partials/'
+  partialsDir: __dirname + '/views/partials/',
+  helpers
 } ) );
+
 
 app.get('/', function (req, res, next) {
 
@@ -24,6 +26,24 @@ app.get('/', function (req, res, next) {
         var contentGraph = amp.inlineContent(response.data);
         stringContent = JSON.stringify(contentGraph,null,'\t');
         res.render('homepage',{'contentGraph': contentGraph, 'stringContent' : stringContent, 'reqParams': req.query});
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.get('/debug', function (req, res, next) {
+
+  try {
+    axios.get("http://qa-tupp-alb-9m2gkk2q44wy-713027552.eu-west-1.elb.amazonaws.com/cms/content/query?fullBodyObject=true&query=%7B%22sys.iri%22:%22http://content.cms.amplience.com/"+ req.query.id +"%22%7D&scope=tree&store=" + req.query.store)
+      .then(response => {
+        var contentGraph = amp.inlineContent(response.data);
+        stringContent = JSON.stringify(contentGraph,null,'\t');
+        console.log(contentGraph);
+        res.render('debug',{'contentGraph': contentGraph, 'stringContent' : stringContent, 'reqParams': req.query});
       })
       .catch(error => {
         console.log(error);
